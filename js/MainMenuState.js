d0bdd1370a6e142d696fbddfdbef1826696e3433
Harry <xuj10@rpi.edu>
2018-10-08 01:02:52 -0400

    Start to implement the main menu state.

--- js/MainMenuState.js
+++ js/MainMenuState.js
@@ -2,7 +2,24 @@ let MainMenuState = function() {
 };
 
 MainMenuState.prototype.preload = function() {
+    this.startButtonPosition = [0.4 * game.width, 0.8 * game.height]
 };
 
 MainMenuState.prototype.create = function() {
+    this.startButton = game.add.button(this.startButtonPosition[0], this.startButtonPosition[1],
+        'StartBotton', hitStartBotton, this);
+    this.startButton.width = 200;
+    this.startButton.height = this.startButton.width;
+    let textConfig = {
+        font: '100px Arial',
+        align: 'center',
+        fontSize: 100,
+        fill: 'white',
+    };
+    this.titleText = game.add.text(200, 200, 'Game Name', textConfig);
+
 };
+
+function hitStartBotton() {
+    alert('Hello world!');
+}
--- js/PreloadState.js
+++ js/PreloadState.js
@@ -2,6 +2,8 @@ let PreloadState = function() {
 };
 
 PreloadState.prototype.preload = function() {
+    game.load.image('StartBotton', 'assets/buttons/StartButtonNormal.jpg');
+    game.load.image('StartButtonOver', 'assets/buttons/StartButtonOver.jpg')
 };
 
 PreloadState.prototype.create = function() {
