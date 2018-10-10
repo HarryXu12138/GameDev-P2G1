1e31aff572e5074be8021826c1330ee5db3e0f76
Harry <xuj10@rpi.edu>
2018-10-10 15:34:41 -0400

    Add go back button to the level selector.

--- js/LevelSelectState.js
+++ js/LevelSelectState.js
@@ -11,15 +11,35 @@ LevelSelectState.prototype.create = function() {
 
 LevelSelectState.prototype.UISettings = function() {
     this.level1ButtonPosition = [0.1 * game.width, 0.1 * game.height];
+    this.level1ButtonShape = [200, 200];
+    this.level1ButtonSprite = 'Level1Button';
+    this.level1ButtonSpriteFrames = [0, 0, 0, 0];
+
+    this.goBackButtonPosition = [0.9 * game.width, 0.05 * game.height];
+    this.goBackButtonShape = [200, 200];
+    this.goBackButtonSprite = 'GoBackButton';
+    this.goBackButtonSpriteFrames = [0, 0, 0, 0];
 };
 
 LevelSelectState.prototype.initializeUI = function() {
-    this.level1Button = game.add.button(this.level1ButtonPosition[0], this.level1ButtonPosition[1],
-        'Level1', this.hitLevel1Button, this);
-    this.level1Button.anchor.setTo(0.5, 0.5);
+    this.addButton(this.level1ButtonPosition, this.level1ButtonShape,
+        this.level1ButtonSprite, this.level1ButtonSpriteFrames, this.hitLevel1Button);
+    this.addButton(this.goBackButtonPosition, this.goBackButtonShape,
+        this.goBackButtonSprite, this.goBackButtonSpriteFrames, this.hitGoBackButton);
+};
+
+LevelSelectState.prototype.addButton = function(position, shape, sprite, spriteFrames, callback) {
+    let newButton = game.add.button(position[0], position[1], sprite, callback, this,
+        spriteFrames[0], spriteFrames[1], spriteFrames[2], spriteFrames[3]);
+    newButton.anchor.setTo(0.5, 0.5);
+    newButton.width = shape[0];
+    newButton.height = shape[1];
+};
+
+LevelSelectState.prototype.hitGoBackButton = function() {
+    game.state.start('MainMenuState');
 };
 
 LevelSelectState.prototype.hitLevel1Button = function() {
-	game.state.start("GameState", true, false, 1);
-    alert('Enter level 1');
+	game.state.start('GameState', true, false, 1);
 };
--- js/PreloadState.js
+++ js/PreloadState.js
@@ -5,7 +5,7 @@ PreloadState.prototype.preload = function() {
     game.load.spritesheet('StartButton', 'assets/buttons/StartButtonSpriteSheet.png', 1289, 1310);
     game.load.bitmapFont('DefaultFont', 'assets/fonts/shortStack.png', 'assets/fonts/shortStack.xml');
     game.load.text('AboutText', 'assets/about.txt');
-    game.load.image('Level1', 'assets/Level1.png');
+    game.load.image('Level1Button', 'assets/buttons/Level1Button.png');
     game.load.image('GoBackButton', 'assets/buttons/GoBackButton.png');
 };
 
