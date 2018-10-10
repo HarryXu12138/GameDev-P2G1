518a2868c38b38be7610c479bd34b61366bb059f
Jordan Faas-Bush <jmanfb@gmail.com>
2018-10-10 16:52:23 -0400

    Notes play and score increases when you tap on notes

--- js/GameState.js
+++ js/GameState.js
@@ -23,12 +23,24 @@ gamePlayState.prototype.create = function(){
 
 	//Set up the group for notes
 	this.notes = game.add.group();
+
+	this.musicManager = new MusicManager(game);
+
+	this.score = 0;
+	this.stunTimer = 0; // if it's greater than zero we count down until it's zero and we aren't stunned
+	this.planeChannel = 2; // start at the middle channel
+
+	// this is for determining plane swipes:
+	// this.events.onInputDown.add(this.cursorDownListener);
+	// this.events.onInputUp.add(this.cursorUpListener);
+	this.pointerDownLoc = null; // this is set by the oninput down and used for swipe determination.
 };
 
 gamePlayState.prototype.update = function(){
 	//If it's been long enough since the last spawn, spawn stars
 	let d = new Date();
 	if((d.getTime() - timeSince >= 1000) && spawning){
+		// this.musicManager.playNote(0, 0, 0);
 		let currentLine = lineInfo[lineNumber];
 		//parse through the current line from the level info doc, spawning a star if there's a 1
 		for(let x = 0; x < currentLine.length; x++){
@@ -37,18 +49,23 @@ gamePlayState.prototype.update = function(){
 				let note = this.notes.create((x*225) + 112.5, 0, "quarternote");
 				note.height = 128;
 				note.width = 128;
+				note.inputEnabled = true;
 				//note.body.gravity.y = 300;
 				//star.playable = true;
 				//star.inputEnabled = true;
 				//star.events.onInputDown.add(listener, star);
 				timeSince = d.getTime();
+
+
+				note.score = 1001;
+
+				note.events.onInputDown.add( (note) => { playNote(this.musicManager, 0, x, 0, 100); this.increaseScore(note); }, this);
 			}
 		}
 		lineNumber++;
 		if(lineNumber >= lineInfo.length){
 			spawning = false;
 		}
-
 	}
 
 	//move the notes, destroying them if they reach the bottom
@@ -63,7 +80,38 @@ gamePlayState.prototype.update = function(){
 	}
 };
 
-playNote = function(note, score) {
+playNote = function(musicManager, instrument, note, duration, score) {
 	// this gets turned into an anonymous function which already has the correct note passed in etc.
-	
+	// also need to determine how close to the beat you are
+	// note is x from left to right
+	// duration is 0 = quarter, 1 = half, 2 = whole
+	musicManager.playNote(0, note, duration);
+}
+
+gamePlayState.prototype.increaseScore = function(note) {
+	this.score += note.score;
+	console.log("Score: " + this.score);
+}
+
+
+gamePlayState.prototype.cursorDownListener = function(pointer) {
+	this.pointerDownLoc = pointer;
+}
+
+gamePlayState.prototype.cursorUpListener = function(pointer) {
+	var dx = pointer.x - this.pointerDownLoc.x;
+	var dy = pointer.y - this.pointerDownLoc.y;
+
+	if (Math.abs(dx) > this.minSwipeDistance) {
+		// then change the planes channel if possible
+		if (dx < 0 && this.planeChannel > 0) {
+			this.planeChannel -= 1;
+			// also set up the animation
+		}
+		else if (dx > 0 && this.planeChannel < 4) {
+			this.planeChannel += 1;
+			// also set up the animation
+		}
+		console.log("Swipe detected. Channel = " + this.planeChannel);
+	}
 }
--- js/PreloadState.js
+++ js/PreloadState.js
@@ -8,7 +8,7 @@ PreloadState.prototype.preload = function() {
     game.load.image("quarternote", "assets/notecloudQuarter.png");
 
     // load audio:
-    game.load.audio('testAudio', 'assets/audio/testNote.mp3');
+    this.loadNotes();
 
     //load level files
     game.load.text("1", "assets/levels/testSpawning.txt");
@@ -17,3 +17,12 @@ PreloadState.prototype.preload = function() {
 PreloadState.prototype.create = function() {
     game.state.start("MainMenuState");
 };
+
+
+PreloadState.prototype.loadNotes = function() {
+	game.load.audio('p_c5_quarter', 'assets/audio/notes/piano_c5_quarter.mp3');
+	game.load.audio('p_d5_quarter', 'assets/audio/notes/piano_d5_quarter.mp3');
+	game.load.audio('p_e5_quarter', 'assets/audio/notes/piano_e5_quarter.mp3');
+	game.load.audio('p_g5_quarter', 'assets/audio/notes/piano_g5_quarter.mp3');
+	game.load.audio('p_a5_quarter', 'assets/audio/notes/piano_a5_quarter.mp3');
+}
